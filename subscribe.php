6b12d927291ef3e85e691b2ab81a7aa885fc9920
Bart van Dorp <bvandorp@gmail.com>
2014-08-04 10:33:37 +0200

    Initial setup

---
+++ subscribe.php
@@ -0,0 +1,28 @@
+<?php
+$nieuwsbrief = $hook->getValue('nieuwsbrief');
+$email = $hook->getValue('email');
+
+if($nieuwsbrief=='ja'){
+
+	$curl = curl_init('https://www.graphicmail.com/api.aspx?Username=username&Password=password&Function=post_subscribe&Email='.$email.'&MailinglistID=123456&SID=4');
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+	// Send the request
+	$result = curl_exec($curl);
+	// Free up the resources $curl is using
+	curl_close($curl);
+
+	$check = explode('|',$result);
+
+	if($check[0]==1){
+		return true;
+	}elseif($check[0]==2){
+		return true;
+	}else{
+		$hook->addError('nieuwsbrief',$check[1]);
+		return false;
+	}
+
+}else{
+	return true;
+}
